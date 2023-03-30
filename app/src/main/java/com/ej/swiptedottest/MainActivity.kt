77ea2e09d7707e4ddb673d070eba66e8fabbe720
김어진 <linexy@naver.com>
2023-03-31 00:58:18 +0900

    Feat: adapter 적용 (에러 추측 : viewpager adapter를 recyclerview adapter로 변경)

--- app/src/main/java/com/ej/swiptedottest/MainActivity.kt
+++ app/src/main/java/com/ej/swiptedottest/MainActivity.kt
@@ -3,13 +3,38 @@ package com.ej.swiptedottest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ej.swiptedottest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val frag1 = BlankFragment1()
+    val frag2 = BlankFragment2()
+    val frag3 = BlankFragment3()
+    val fragList = arrayOf(frag1, frag2, frag3)
+
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.lifecycleOwner = this
+
+
+        val viewPager = binding.viewPager
+        val circleIndicator = binding.circleIndicator3
+
+        val tabAdapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return fragList.size
+            }
+            override fun createFragment(position: Int): Fragment {
+                return fragList[position]
+            }
+        }
+        circleIndicator.setViewPager(viewPager)
+        viewPager.adapter = tabAdapter
+
+
     }
 }
