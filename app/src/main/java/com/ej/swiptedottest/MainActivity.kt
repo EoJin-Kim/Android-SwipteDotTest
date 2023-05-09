26f900f00c556d24042f4fd58b21a76c66e70de8
linexy <linexy@naver.com>
2023-05-09 17:42:18 +0900

    Feat: fragment 여러개 추가

--- app/src/main/java/com/ej/swiptedottest/MainActivity.kt
+++ app/src/main/java/com/ej/swiptedottest/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.ej.swiptedottest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.ej.swiptedottest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     val frag1 = BlankFragment1()
     val frag2 = BlankFragment2()
     val frag3 = BlankFragment3()
-    val fragList = arrayOf(frag1, frag2, frag3)
+    val frag4 = BlankFragment3()
+    val frag5 = BlankFragment3()
+    val frag6 = BlankFragment3()
+    val frag7 = BlankFragment3()
+    val fragList = arrayOf(frag1, frag2, frag3,frag4,frag5,frag6,frag7)
 
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
